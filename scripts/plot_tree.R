0ab3b3474e92410273777ea3016483586df5cba1
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2022-10-31 16:36:01 -0400

    Add R scripts to plot trees

---
+++ scripts/plot_tree.R
@@ -0,0 +1,83 @@
+#!/usr/bin/env Rscript
+
+# SET-UP -----------------------------------------------------------------------
+# remotes::install_version("dplyr", version = "1.0.5")
+# remotes::install_github("YuLab-SMU/tidytree")
+# remotes::install_github("YuLab-SMU/ggtree")
+
+## Packages
+if (!"pacman" %in% installed.packages())
+  install.packages("pacman", repos='http://cran.us.r-project.org')
+if (!"BiocManager" %in% installed.packages())
+  install.packages("BiocManager", repos='http://cran.us.r-project.org')
+if (!"ggtree" %in% installed.packages()) BiocManager::install("ggtree")
+packages <- c("tidyverse", "here", "ape", "ggtree", "argparse", "readxl")
+pacman::p_load(char = packages, install = TRUE, repos = 'http://cran.us.r-project.org')
+
+## Define input files
+meta_file <- "results/summary of the serotype_WGS_salmonella.xlsx"
+meta_ref_file <- "results/patric_refs/lookup.txt"
+tree_file <- "results/ksnp3/with_refs/tree.core.tre"
+  
+## Define output files
+tree_plotfile <- "results/ksnp3/tree.png"
+
+
+# PLOT THE TREE ----------------------------------------------------------------
+## Read and prep metadata
+meta_ref <- read_tsv(meta_ref_file, show_col_types = FALSE) %>% 
+  select(sampleID = accession, serotype) %>%
+  mutate(serotype = str_to_title(serotype),
+         serotype = sub("Branderup", "Brenderup", serotype))
+
+meta <- read_xlsx(meta_file) %>%
+  mutate(sampleID = sub("AG21-0", "", `Sample number`)) %>%
+  filter(sampleID != "Average") %>% 
+  select(sampleID, serotype = Serotype) %>%
+  bind_rows(meta_ref)
+
+## Read the tree file and prep the tree
+tree <- read.tree(tree_file)
+tree$tip.label <- sub("AG21-0", "", tree$tip.label)
+nseqs <- length(tree$tip.label)
+tree$node.label <- round(as.numeric(tree$node.label) * 100)
+
+## Define colors
+#randomcoloR::distinctColorPalette(20)
+mycols <- c("#a74bb4", "#62b54f", "#7064d3", "#b5b348", "#dd6fc5",
+            "#4db18a", "#ce416e", "#45aecf", "#d55035", "#7784cb",
+            "#cc8e3e", "#ac6090", "#647934", "#df837e", "#9c5736")
+sero_cols <- mycols[1:length(unique(meta$serotype))]
+
+## Make the plot
+p <- ggtree(tree, layout = "rectangular") %<+% meta +    # or layout = "circular"
+        geom_tiplab(aes(color = serotype), size = 2.5) +
+        geom_treescale(x = 0, y = nseqs - 1, color = "grey50") +
+        geom_rootedge(rootedge = 0.005) +
+        scale_color_manual(values = sero_cols) +
+        theme(plot.margin = margin(0.2, 2, 0.2, 0.2, "cm"),
+              legend.position = "left") +
+        coord_cartesian(clip = "off")
+p
+
+ggtree(tree, layout = "rectangular") %<+% meta +    # or layout = "circular"
+  geom_tiplab(aes(color = serotype), size = 2.5, offset = 0.001) +
+  theme_tree2() +
+  geom_rootedge(rootedge = 0.005) +
+  #scale_color_manual(values = sero_cols) +
+  theme(plot.margin = margin(0.2, 2, 0.2, 0.2, "cm"),
+        legend.position = "left") +
+  coord_cartesian(clip = "off") +
+  annotate("text", x = 0.66, y = nseqs + 2, label = "Bla") +
+  annotate("text", x = 0.75, y = nseqs + 2, label = "Bla2") +
+  geom_text2(aes(label = label, subset = !isTip & as.numeric(label) != 100),
+             hjust = 1, vjust = 1, nudge_x = -0.005,
+             size = 2.5, fontface = "italic", color = "grey50")
+
+ggsave(tree_plotfile, p)
+
+## Add bootstrap values:
+# geom_text2(aes(label = label,
+#                subset = !is.na(as.numeric(label)) & as.numeric(label) > 95),
+#            nudge_x = -0.0027, nudge_y = 0.17,
+#            size = 4, fontface = "italic", color = "grey50")
